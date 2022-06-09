657d45093a38043615d3e90110e45e1228f25c52
Karpov Yuri <yurick@bk.ru>
2022-06-09 12:10:56 +0300

    fix: Исправить работу ТаймЛайнов

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const startTimer = 5;
+// Установка начального значения счетчика
+const startTimer = 20;
 let timer = startTimer - 1;
-const leftTimeLineWidth = 33;
-const rightTimeLineWidth = 42;
+
+// Устанавливаем размеры таймлайнов
+const timeLineWidth = 33;
 const timeLineHeight = 280;
+
+
 const counts = document.querySelectorAll('.timer');
 const timeLines = document.querySelectorAll('.time-line__svg-img');
 const button = document.querySelector('.button');
@@ -11,8 +15,11 @@ let mirror = 1;
 let countDown = null;
 
 
+// Функция запуска обратного отсчета
 function startCountDown() {
   countDown = setInterval(() => {
+
+    // Деактивируем кнопку
     if (timer < 1) {
       button.style.cursor = `default`;
       button.removeEventListener('click', handleClick);
@@ -20,43 +27,70 @@ function startCountDown() {
     if (timer < 0) {
       clearInterval(countDown);
     } else {
+
+      // Перерисовываем счетчик
       counts.forEach((item) => {
         item.style.color = "#FF0059";
         item.textContent = timer;
       });
+
+      // Вычисляем текущую высоту красного таймлайна
       const height = (startTimer - timer) * timeLineHeight / startTimer;
-      timeLines.forEach(item => item.setAttribute('height', `${height}`));
-      timeLines[0].setAttribute('viewBox', `0 ${(timeLineHeight - height) / 2} ${leftTimeLineWidth} ${height}`);
-      timeLines[1].setAttribute('viewBox', `0 ${(timeLineHeight - height) / 2} ${rightTimeLineWidth} ${height}`);
+
+      // Отрисовываем красный таймлайн
+      timeLines.forEach(item => {
+        item.setAttribute('height', `${height}`);
+        item.setAttribute('viewBox', `0 ${(timeLineHeight - height) / 2} ${timeLineWidth} ${height}`);
+      });
     }
     timer -= 1;
   }, 1000);
 }
 
 
+// Функция обработки события кнопки
 function handleClick() {
   clearInterval(countDown);
+
+  // Имитируем загрузку нового видео
   video.style.transform = `scale(${-mirror}, 1)`;
   mirror = -mirror;
+
+  // Устанавливаем начальное значение и цвет счетчиков
   counts.forEach((item) => {
     item.style.color = "#fff";
     item.textContent = startTimer;
   });
-  timer = startTimer - 1;
-  timeLines.forEach(item => item.setAttribute('height', `0`));
-  timeLines[0].setAttribute('viewBox', `0 0 0 0`);
-  timeLines[1].setAttribute('viewBox', `0 0 0 0`);
+
+  // Устанавливаем начальное значение таймлайнов
+  timeLines.forEach(item => {
+    item.setAttribute('height', `0`);
+    item.setAttribute('viewBox', `0 0 0 0`);
+  });
+
+  // Перезапускаем анимацию кнопки
   button.classList.remove('animation');
   void button.offsetWidth;
   button.classList.add('animation');
+
+  // Перезапускаем счетчик
+  timer = startTimer - 1;
   startCountDown();
 }
 
 
-button.style.animationDuration = `${startTimer}s`;
+// Устанавливаем обработчик события на кнопку
 button.addEventListener('click', handleClick);
+
+// Устанавливаем начальное значение счетчиков
 counts.forEach(item => item.textContent = startTimer);
-timeLines[0].setAttribute('width', `${leftTimeLineWidth}`);
-timeLines[1].setAttribute('width', `${rightTimeLineWidth}`);
+
+// Устанавливаем ширину Тайм лайнов
+timeLines.forEach(item => item.setAttribute('width', `${timeLineWidth}`));
+
+// Запускаем анимацию кнопки
+button.style.animationDuration = `${startTimer}s`;
 button.classList.add('animation');
+
+// Запускаем обратный отсчет
 startCountDown();
